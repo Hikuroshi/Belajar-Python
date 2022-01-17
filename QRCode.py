d84a5a02d3bf5631cdd497e66924a1c9202c99b0
Hikuroshi <92168188+Hikuroshi@users.noreply.github.com>
2022-01-17 10:16:02 +0700

    first commit

---
+++ QRCode.py
@@ -0,0 +1,54 @@
+# Importing library
+import qrcode
+import cv2
+import random
+import string
+
+# Data to encode
+def linkQR():
+	data = input("Masukan Link: ")
+	# Creating an instance of QRCode class
+	qr = qrcode.QRCode(
+		version = 1,
+		box_size = 10,
+		border = 5)
+
+	# Adding data to the instance 'qr'
+	qr.add_data(data)
+
+	qr.make(fit = True)
+	img = qr.make_image(
+		fill_color = 'black',
+		back_color = 'white')
+	
+	lower = string.ascii_lowercase
+	upper = string.ascii_uppercase
+	num = string.digits
+
+	all = lower + upper + num
+	
+	namr = "QRcd.png"
+	panjnam = int(5)
+	nama = random.sample(all, panjnam)
+	namas = ''.join(nama)
+
+	img.save('{}{}'.format(namas, namr))
+
+def QRlink():
+	img = cv2.imread(input("Masukan Image: "))
+	det = cv2.QRCodeDetector()
+	val = det.detectAndDecode(img)
+	print(val)
+
+
+print("Pilih Menu")
+print("1.Mengubah link menjadi QR-Code")
+print("2.Mengubah QR-Code menjadi link")
+
+pilih = input("Masukan 1/2 untuk memilih: ")
+
+
+if pilih == '1':
+	print(linkQR())
+elif pilih == '2':
+	print(QRlink())
